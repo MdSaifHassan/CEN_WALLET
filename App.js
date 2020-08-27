9225622649053e4557998003751431f50896da96
MdSaifHassan <64311141+MdSaifHassan@users.noreply.github.com>
2020-08-27 15:09:19 +0530

    Redux Update

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import NavigationScreen from './src/navigation/Navigation';
-
 const Stack = createStackNavigator();
+import { Provider } from 'react-redux';
+import {store} from './redux/store';
+
+const store = configureStore();
 
 function App() {
   return (
+    <Provider store={store}>
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={NavigationScreen} />
       </Stack.Navigator>
     </NavigationContainer>
+    </Provider>
   );
 }
 
