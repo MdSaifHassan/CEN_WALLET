7e7e3f9d6df12d7721d0288cd0159c5a480cd359
MdSaifHassan <64311141+MdSaifHassan@users.noreply.github.com>
2020-08-27 12:09:44 +0530

    helo

--- App.js
+++ App.js
@@ -1,75 +1,20 @@
 import * as React from 'react';
-import { Button, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from './src/components/home/index'
-import Login from './src/components/login/Login1';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import LinkAccount from './src/components/link crad/LinkAccount';
-import { AntDesign } from '@expo/vector-icons'; 
+import NavigationScreen from './src/navigation/Navigation';
 
 
-const HomeStack = createStackNavigator();
-const SettingsStack = createStackNavigator();
+const Stack = createStackNavigator();
 
-function HomeStackScreen() {
-  return (
-    <HomeStack.Navigator>
-      <HomeStack.Screen name="Home" component={HomeScreen} />
-    </HomeStack.Navigator>
-  );
-}
-// //////
-function LoginStackScreen() {
-  return (
-    <SettingsStack.Navigator>
-      <SettingsStack.Screen name="Login" component={Login} />
-    </SettingsStack.Navigator>
-  );
-}
-function LinkAccountStackScreen() {
-  return (
-    <SettingsStack.Navigator>
-      <SettingsStack.Screen name="Paymenet" component={LinkAccount} />
-    </SettingsStack.Navigator>
-  );
-}
-
-const Tab = createBottomTabNavigator();
-
-export default function NavigationScreen() {
+function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-
-            if (route.name === 'Home') {
-              iconName = focused
-                ? 'ios-home'
-                : 'ios-home';
-            } else if (route.name === 'Login') {
-              iconName = focused ? 'ios-gift' : 'ios-gift';
-            }else if (route.name === 'PayMent') {
-              iconName = focused ? 'ios-payment' : 'ios-payment';
-            }
-            return(
-             <Ionicons name={iconName} size={size} color={color} />
-            )
-         
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: 'tomato',
-          inactiveTintColor: 'black',
-        }}>
-        <Tab.Screen name="Home" component={HomeStackScreen} />
-        <Tab.Screen name="Login" component={LoginStackScreen} />
-        <Tab.Screen name="PayMent" component={LinkAccountStackScreen} />
-
-      </Tab.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={NavigationScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
---
+++ src/navigation/Navigation.js
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import { Button, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../home/index'
+import Login from '../login/Login1';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import LinkAccount from './src/components/link crad/LinkAccount';
+import { AntDesign } from '@expo/vector-icons'; 
+
+
+const HomeStack = createStackNavigator();
+const SettingsStack = createStackNavigator();
+
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+    </HomeStack.Navigator>
+  );
+}
+// //////
+function LoginStackScreen() {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name="Login" component={Login} />
+    </SettingsStack.Navigator>
+  );
+}
+function LinkAccountStackScreen() {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name="Paymenet" component={LinkAccount} />
+    </SettingsStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function NavigationScreen() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-home'
+                : 'ios-home';
+            } else if (route.name === 'Login') {
+              iconName = focused ? 'ios-gift' : 'ios-gift';
+            }else if (route.name === 'PayMent') {
+              iconName = focused ? 'ios-payment' : 'ios-payment';
+            }
+            return(
+             <Ionicons name={iconName} size={size} color={color} />
+            )
+         
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'black',
+        }}>
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="Login" component={LoginStackScreen} />
+        <Tab.Screen name="PayMent" component={LinkAccountStackScreen} />
+
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
